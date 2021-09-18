a6a4bec2208780729654514729ca1ec483d66840
Nitemice <webmaster@nitemice.net>
2021-09-18 18:57:38 +1000

    Swap to using common GAS functions

--- code.js
+++ code.js
@@ -1,29 +1,5 @@
 const mapUrl = "https://www.google.com/maps/d/kml?forcekml=1&mid=";
 
-
-function findOrCreateFile(parentDir, filename, content)
-{
-    var file;
-
-    // See if there's already a file in the indicated Google Drive folder
-    var backupFolder = DriveApp.getFolderById(parentDir);
-    var files = backupFolder.getFilesByName(filename);
-    if (files.hasNext())
-    {
-        file = files.next();
-        // Set the file contents
-        file.setContent(content);
-        Logger.log("Updated existing file: " + filename);
-    }
-    else
-    {
-        // Create a new file with content
-        file = backupFolder.createFile(filename, content);
-        Logger.log("Created new file: " + filename);
-    }
-    return file;
-}
-
 function downloadMap(backupDir, mapId, mapName)
 {
     // Append Map ID to URL
@@ -34,7 +10,8 @@ function downloadMap(backupDir, mapId, mapName)
     //Logger.log(response);
 
     // Save the map file in the indicated Google Drive folder
-    var file = findOrCreateFile(backupDir, mapName + ".kml", response.getContentText());
+    var file = common.updateOrCreateFile(backupDir, mapName + ".kml",
+        response.getContentText());
 }
 
 function main()
